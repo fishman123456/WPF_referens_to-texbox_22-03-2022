dd535eaca1289358dcea529fe344178d03a4dae5
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-22 19:58:21 +0700

    Initial commit

---
+++ Class_ref.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_referens_to_texbox_22_03_2022
+{
+    internal class Class_ref
+    {
+
+        public static void text_ref(ref string text)
+        {
+            
+        }
+    }
+}
